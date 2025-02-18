cc6479b3a9424b2d1b5d13fd134ab1306b61bc4f
Breno Norberto <breno@MacBook-Air-de-Fran.local>
2025-02-18 19:49:40 -0300

    feat: [fullstackweek-donalds] add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import React from "react"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Products Page</h1>
+      <Button>Button</Button>
+      <Input />
+    </div>
+  )
+}
+
+export default ProductsPage
