7a2178a4dee919f2f7aed587c220fb7b4968ef0e
Breno Norberto <brenonorberto@gmail.com>
2025-02-18 20:14:12 -0300

    chore: [fullstackweek-donalds] update eslint sort and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,17 @@
-"use client"
-import React from "react"
+"use client";
+import React from "react";
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const ProductsPage = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-4 p-5">
       <h1 className="text-red-500">Products Page</h1>
       <Button>Button</Button>
       <Input />
     </div>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
